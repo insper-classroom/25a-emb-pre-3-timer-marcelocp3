b6c34cfe5ff74057bfff6a1ca96d36729f415e40
marcelocp3 <marcelocp3@al.insper.edu.br>
2025-03-09 18:52:49 -0300

    teste3.1

--- exe3/main.c
+++ exe3/main.c
@@ -1,5 +1,5 @@
-#include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
@@ -8,11 +8,10 @@ const int LED_PIN_R = 4;
 volatile int flag_f_r = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
+    if (events == 0x4) { 
         if (gpio == BTN_PIN_R)
             flag_f_r = 1;
-
-    } else if (events == 0x8) { // rise edge
+    } else if (events == 0x8) {
         if (gpio == BTN_PIN_R)
             flag_f_r = 0;
     }
@@ -23,17 +22,33 @@ int main() {
 
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0);
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
 
     gpio_set_irq_enabled_with_callback(
-        BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
+        BTN_PIN_R,
+        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE,
+        true,
+        &btn_callback
+    );
+
+    bool led_ligado = false;
+    int contador = 0;
 
     while (true) {
-
         if (flag_f_r) {
+            contador++;
+        } else {
+            if (contador >= 500) {
+                led_ligado = !led_ligado;
+                gpio_put(LED_PIN_R, led_ligado);
+            }
+            contador = 0;
         }
+        sleep_ms(1);
     }
+    return 0;
 }
