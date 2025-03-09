86aa96b6dfd620c860dbafb944a9be755adc6502
marcelocp3 <marcelocp3@al.insper.edu.br>
2025-03-09 18:25:50 -0300

    teste2.1

--- exe1/main.c
+++ exe1/main.c
@@ -1,16 +1,18 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
 
-#define BOTAO_PINO 28
-#define LED_PINO 4
+const int BOTAO_PINO 28
+const int LED_PINO 4
 
-volatile bool botao_pressionado = false;
+volatile int flag_f_r = 0;
 volatile bool timer_init = false;
 
 void botao_callback(uint gpio, uint32_t eventos) {
-    if (eventos & GPIO_IRQ_EDGE_FALL) {
-        botao_pressionado = true;
+    if (eventos == 0x4) {
+        flag_f_r = 1;
+    } else if (eventos == 0x8) {
     }
 }
 
@@ -28,19 +30,19 @@ int main(void) {
     gpio_init(LED_PINO);
     gpio_set_dir(LED_PINO, GPIO_OUT);
     gpio_put(LED_PINO, 0);
-
     gpio_init(BOTAO_PINO);
     gpio_set_dir(BOTAO_PINO, GPIO_IN);
     gpio_pull_up(BOTAO_PINO);
-
-    gpio_set_irq_enabled_with_callback(BOTAO_PINO,
-                                       GPIO_IRQ_EDGE_FALL,
-                                       true,
-                                       &botao_callback);
+    gpio_set_irq_enabled_with_callback(
+        BOTAO_PINO,
+        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE,
+        true,
+        botao_callback
+    );
 
     while (true) {
-        if (botao_pressionado) {
-            botao_pressionado = false;
+        if (flag_f_r) {
+            flag_f_r = 0;
             if (!timer_ativo) {
                 add_repeating_timer_ms(500, timer_callback, NULL, &meu_timer);
                 timer_ativo = true;
--- exe2/main.c
+++ exe2/main.c
@@ -1,18 +1,17 @@
-#include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
-
 const int LED_PIN_R = 4;
 const int LED_PIN_G = 6;
 
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
-void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) {
+void btn_callback(uint gpio, uint32_t eventos) {
+    if (eventos == 0x4) {
         if (gpio == BTN_PIN_R)
             flag_r = 1;
         else if (gpio == BTN_PIN_G)
@@ -21,32 +20,81 @@ void btn_callback(uint gpio, uint32_t events) {
 }
 
 int main() {
+    stdio_init_all();
+    
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
-
+    gpio_put(LED_PIN_R, 0);
+    
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
-
+    gpio_put(LED_PIN_G, 0);
+    
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
-
+    
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
-
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+    
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
-
+    
+    bool timer_r = false;
+    bool timer_g = false;
+    bool led_r = false;
+    bool led_g = false;
+    int contador_r = 0;
+    int contador_g = 0;
+    
     while (true) {
-
         if (flag_r) {
             flag_r = 0;
+            if (!timer_r) {
+                timer_r = true;
+                led_r = false;
+                gpio_put(LED_PIN_R, led_r);
+                contador_r = 0;
+            } else {
+                timer_r = false;
+                led_r = false;
+                gpio_put(LED_PIN_R, led_r);
+                contador_r = 0;
+            }
         }
-
         if (flag_g) {
             flag_g = 0;
+            if (!timer_g) {
+                timer_g = true;
+                led_g = false;
+                gpio_put(LED_PIN_G, led_g);
+                contador_g = 0;
+            } else {
+                timer_g = false;
+                led_g = false;
+                gpio_put(LED_PIN_G, led_g);
+                contador_g = 0;
+            }
         }
+        if (timer_r) {
+            contador_r++;
+            if (contador_r >= 500) {
+                led_r = !led_r;
+                gpio_put(LED_PIN_R, led_r);
+                contador_r = 0;
+            }
+        }
+        if (timer_g) {
+            contador_g++;
+            if (contador_g >= 250) {
+                led_g = !led_g;
+                gpio_put(LED_PIN_G, led_g);
+                contador_g = 0;
+            }
+        }
+        sleep_ms(1);
     }
+    
+    return 0;
 }
